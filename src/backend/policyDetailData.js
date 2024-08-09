8c44ca6f84b887d6837c15da39611aa1a55c03ec
Peter Heung <chunyipheung@gmail.com>
2024-08-09 11:04:16 +0800

    Auto commit

--- src/backend/policyDetailData.js
+++ src/backend/policyDetailData.js
@@ -3,6 +3,7 @@ import { policyDetailData_TRL_HiEnd } from "./policyDetailData_TRL_HiEnd";
 export const policyDetailData = async (type,policyno) => {
     var list = [];
     var test = [];
+    var test3 = [];
     if(type === "TRL-HiEnd"){
         var res = await policyDetailData_TRL_HiEnd("offline",policyno);
         if(res.overview.length == 6){
