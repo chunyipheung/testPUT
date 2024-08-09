2b952f362a758206fc37e4a29cdac9a7f333a858
Peter Heung <chunyipheung@gmail.com>
2024-08-09 12:16:51 +0800

    Test

--- src/backend/policyDetailData.js
+++ src/backend/policyDetailData.js
@@ -2,7 +2,7 @@ import { policyDetailData_TRL_HiEnd } from "./policyDetailData_TRL_HiEnd";
 
 export const policyDetailData = async (type,policyno) => {
     var list = [];
-    var test24 = [];
+    var test244 = [];
     var test2434 = [];
     if(type === "TRL-HiEnd"){
         var res = await policyDetailData_TRL_HiEnd("offline",policyno);
